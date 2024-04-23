fa318bf67dce0efe45539dbc85664b2d57ea0c39
coldinke <yangzhibo.yang@gmail.com>
2024-04-23 23:55:37 +0800

    fix(senosr): fix some errors
    - Class.__init__ funcation parameters missmatch
    - add Camera class for project.

--- src/sensor.py
+++ src/sensor.py
@@ -1,9 +1,11 @@
 import adafruit_dht
+import picamera
 import board
+
 class Sensor:
     def __init__(self, name, pin):
         self.name = name
-        self.pin = self.pin
+        self.pin = pin
     
     def read(self):
         pass
@@ -15,24 +17,27 @@ class Sensor:
         pass
 
 class DHT11(Sensor):
-    def __init__(self):
-        super().__init__()
+    '''
+        DHT11 sensor class
+    '''
+    def __init__(self, name, pin):
+        super().__init__(name, pin)
         self.operation =  adafruit_dht.DHT11(self.pin)
         self.temperature = 0.0
         self.humidity = 0.0
 
     def read(self):
         self.temperature = self.operation.temperature
-        self.humidity = self.operation.temperature
+        self.humidity = self.operation.humidity
         return (self.temperature, self.humidity)
 
     def close(self):
         self.operation.exit()
 
 class PCF8591(Sensor):
-    def __init__(self, name, addr, smbusNumber):
-        self.name = self.name
-        self.address = addr
+    def __init__(self, name, pin, smbusNumber):
+        super().__init__(name, pin)
+        self.address = self.pin
         self.busNumber = smbusNumber
         self.adc_dac_bus = smbus.SMbus(self.busNumber)
 
@@ -55,4 +60,19 @@ class PCF8591(Sensor):
         self.adc_dac_bus.write_byte_data(self.address, 0x40, temp)
         
         
+class Camera:
+    def __init__(self):
+        self.camera = picamera.PiCamera()
+
+    def capture(self, output_path):
+        self.camera.capture(output_path)
     
+    def record(self, output_path, duration):
+        self.camera.start_recording(output_path)
+        self.camera.wait_recording(duration)
+        self.camera.stop_recording()
+    
+    def close(self):
+        self.camera.close()
+
+
